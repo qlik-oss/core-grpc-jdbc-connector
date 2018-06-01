2e103b7c0dc004b037f0578c2ecfb32d3b872f02
Stefan Enberg <Stefan.Enberg@qlik.com>
2018-06-01 14:05:39 +0200

    Latest with type conversion

--- src/main/java/qlik/jdbc/connector/ConnectorImpl.java
+++ src/main/java/qlik/jdbc/connector/ConnectorImpl.java
@@ -4,11 +4,13 @@ import com.google.protobuf.Descriptors;
 import io.grpc.*;
 import io.grpc.stub.StreamObserver;
 import qlik.connect.ConnectorGrpc;
+import qlik.connect.GrpcServer;
 import qlik.connect.GrpcServer.*;
 
 
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.sql.*;
 
 public class ConnectorImpl
@@ -16,16 +18,37 @@ public class ConnectorImpl
 
     public ThreadLocal<GetDataResponse> initialMetadata = new ThreadLocal<GetDataResponse>();
 
+    private GetDataResponse getDataResponseHeader(ResultSetMetaData rsmd) throws SQLException {
+
+        GetDataResponse.Builder builder = GetDataResponse.newBuilder();
+
+        for(int j = 1 ; j <= rsmd.getColumnCount(); j++){
+            builder.addFieldInfo(FieldInfo.newBuilder()
+                    .setName(rsmd.getColumnName(j))
+                    .setSemanticTypeValue(TypeConversionUtil.getSymanticType(rsmd.getColumnType(j)))
+                    .setFieldAttributes(
+                            FieldAttributes.newBuilder().
+                                    setTypeValue(TypeConversionUtil.getFieldAttrType(rsmd.getColumnType(j)))
+                                    .build()
+                    )
+            ).build();
+        }
+
+        return builder.build();
+    }
+
     public void getData(DataRequest request, StreamObserver<DataChunk> responseObserver) {
         String str = request.getConnection().getConnectionString();
         String sql = request.getParameters().getStatement();
+
+        System.out.println(sql);
 
         Connection conn = null;
         Statement stmt = null;
 
         try{
+            //TODO: Make driver configurable from the outside
 
-            Class.forName("org.postgresql.Driver");
 
             System.out.println("Connecting to database...");
             //TODO: The connection info should be exstracted from the connection string
@@ -33,63 +56,96 @@ public class ConnectorImpl
 
             stmt = conn.createStatement();
 
-
-            DataChunk.Builder d;
-
             ResultSet rs = stmt.executeQuery(sql);
-
             ResultSetMetaData rsmd = rs.getMetaData();
 
-            GetDataResponse.Builder builder = GetDataResponse.newBuilder();
-
-            for(int j = 1 ; j <= rsmd.getColumnCount(); j++){
-
-                builder.addFieldInfo(FieldInfo.newBuilder()
-                        .setName(rsmd.getColumnName(j))
-                        .setSemanticTypeValue(TypeConversionUtil.getSymanticType(rsmd.getColumnTypeName(j)))
-                        .setFieldAttributes(
-                                FieldAttributes.newBuilder().
-                                        setTypeValue(TypeConversionUtil.getFieldAttrType(rsmd.getColumnTypeName(j)))
-                                        .build()
-                        )
-                ).build();
-
-                System.out.print("Name: "+ rsmd.getColumnName(j));
-                System.out.println(" - type: "+ rsmd.getColumnTypeName(j));
-            }
-
-            initialMetadata.set(builder.build());
-
-
-
+            GetDataResponse dataResponse = getDataResponseHeader(rsmd);
+            initialMetadata.set(dataResponse);
 
             int columnCount = rsmd.getColumnCount();
 
-            while(rs.next()){
-                d = DataChunk.newBuilder();
+            DataChunk.Builder dataChunkBuilder;
 
-                for(int i = 1; i <= columnCount; i++){
+            dataChunkBuilder = DataChunk.newBuilder();
+
+            int NR_OF_ROWS = 200;
+            int rowCount = 0;
+
+            while(rs.next()){
+                rowCount++;
+
+                for(int i = 0; i < columnCount; i++){
 
                     try {
                         //TODO: Cleanup this and make an extract logic to its own thread
-                        if (rsmd.getColumnTypeName(i) == "varchar") {
-                            d.addStringBucket(rs.getString(i));
-                            d.addStringCodes(i);
-                        } else if (rsmd.getColumnTypeName(i) == "int4") {
-                            d.addDoubleBucket(rs.getInt(i));
-                            d.addNumberCodes(i);
+                        //need to looka at the orignal type
+//
+//                        if(rsmd.getColumnType(i+1) == Types.CHAR){
+//                            rs.getDouble(i+1 );
+//                            rs.getFloat( i+1 );
+//                        }
+
+                        //System.out.println(rsmd.getColumnType(i+1) + " - " + dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue());
+
+//                        rsmd.getColumnType(i)
+                        if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TEXT_VALUE) {
+                            TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i+1));
+                        }
+
+                        else if (rsmd.getColumnType(i+1) == Types.TINYINT ){
+                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getInt(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.INTEGER ){
+                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getInt(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.BIGINT ){
+                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getLong(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.BOOLEAN){
+                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getBoolean(i+1) ? 1 : 0);
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.BIT){
+                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getBoolean(i+1) ? 1 : 0);
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.DECIMAL ){
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.NUMERIC ){
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.DOUBLE ){
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.FLOAT ){
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getFloat(i+1));
+                        }
+                        else if (rsmd.getColumnType(i+1) == Types.REAL ){
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getFloat(i+1));
+                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.DATE_VALUE) {
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDate(i+1).getTime() / 1000);
+                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TIME_VALUE) {
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTime(i+1).getTime() / 1000);
+                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TIMESTAMP_VALUE) {
+                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTimestamp(i+1).getTime() / 1000);
                         } else {
-                            d.addStringBucket(rs.getString(i));
-                            d.addStringCodes(i);
+                            TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i+1));
                         }
                     }
                     catch(Exception e){
-                        d.addStringBucket("-");
-                        d.addStringCodes(i);
+                        e.printStackTrace();
+                        throw e;
                     }
                 }
 
-                responseObserver.onNext(d.build());
+                if(rowCount % NR_OF_ROWS == 0){
+                    responseObserver.onNext(dataChunkBuilder.build());
+                    dataChunkBuilder = DataChunk.newBuilder();
+                }
+            }
+
+            //Send the remainder of rows
+            if(rowCount % NR_OF_ROWS != 0){
+                responseObserver.onNext(dataChunkBuilder.build());
             }
 
             rs.close();
@@ -119,6 +175,8 @@ public class ConnectorImpl
     }
 
     public GetDataResponse getDataResponseMetadata(){
+
+        System.out.println("Fetching header");
         return initialMetadata.get();
     }
 }
--- src/main/java/qlik/jdbc/connector/TypeConversionUtil.java
+++ src/main/java/qlik/jdbc/connector/TypeConversionUtil.java
@@ -2,53 +2,104 @@ package qlik.jdbc.connector;
 
 import qlik.connect.GrpcServer;
 
+import java.sql.Types;
+
 public class TypeConversionUtil {
-    static public int getFieldAttrType(String type){
-        switch (type){
-            case "varchar":
+    static public int getFieldAttrType(int type){
+        switch (type) {
+            case Types.CHAR:
+            case Types.VARCHAR:
+            case Types.LONGNVARCHAR:
+            case Types.LONGVARCHAR:
+            case Types.NCHAR:
+            case Types.NVARCHAR:
                 return GrpcServer.FieldAttrType.TEXT_VALUE;
-            case "text":
-                return GrpcServer.FieldAttrType.TEXT_VALUE;
-            case "int2":
+
+            case Types.INTEGER:
+            case Types.BIGINT:
+            case Types.BIT:
+            case Types.BOOLEAN:
+            case Types.SMALLINT:
+            case Types.TINYINT:
                 return GrpcServer.FieldAttrType.INTEGER_VALUE;
-            case "int4":
-                return GrpcServer.FieldAttrType.INTEGER_VALUE;
-            case "int8":
-                return GrpcServer.FieldAttrType.INTEGER_VALUE;
-            case "char":
-                return GrpcServer.FieldAttrType.INTEGER_VALUE;
-            case "oid":
-                return GrpcServer.FieldAttrType.INTEGER_VALUE;
-            case "float4":
+            case Types.NUMERIC:
+            case Types.DECIMAL:
+            case Types.DOUBLE:
+            case Types.FLOAT:
+            case Types.REAL:
                 return GrpcServer.FieldAttrType.REAL_VALUE;
-            case "float8":
-                return GrpcServer.FieldAttrType.REAL_VALUE;
-            case "timestamp":
-                return GrpcServer.FieldAttrType.TIMESTAMP_VALUE;
-            case "timestamptz":
-                return GrpcServer.FieldAttrType.TIMESTAMP_VALUE;
-            case "date":
+
+            case Types.DATE:
                 return GrpcServer.FieldAttrType.DATE_VALUE;
-            case "numeric":
-                return GrpcServer.FieldAttrType.REAL_VALUE;
-            case "decimal":
-                return GrpcServer.FieldAttrType.REAL_VALUE;
-            case "bool":
-                return GrpcServer.FieldAttrType.INTEGER_VALUE;
+            case Types.TIME:
+                return GrpcServer.FieldAttrType.TIME_VALUE;
+            case Types.TIMESTAMP:
+                return GrpcServer.FieldAttrType.TIMESTAMP_VALUE;
+
+            case Types.ARRAY:
+            case Types.BINARY:
+            case Types.BLOB:
+            case Types.CLOB:
+            case Types.DATALINK:
+            case Types.DISTINCT:
+            case Types.JAVA_OBJECT:
+            case Types.LONGVARBINARY:
+            case Types.NCLOB:
+            case Types.NULL:
+            case Types.OTHER:
+            case Types.REF:
+            case Types.ROWID:
+            case Types.SQLXML:
+            case Types.STRUCT:
+            case Types.VARBINARY:
+                return GrpcServer.FieldAttrType.UNKNOWN_VALUE;
+
         }
-        return 0;
+
+        return GrpcServer.FieldAttrType.UNKNOWN_VALUE;
     }
 
-    static public int getSymanticType(String type){
+    static public int getSymanticType(int type){
         switch (type){
-            case "timestamp":
-                return GrpcServer.SemanticType.UNIX_SECONDS_SINCE_1970_UTC_VALUE;
-            case "timestamptz":
-                return GrpcServer.SemanticType.UNIX_SECONDS_SINCE_1970_UTC_VALUE;
-            case "date":
+            case Types.DATE:
+            case Types.TIME:
+            case Types.TIMESTAMP:
                 return GrpcServer.SemanticType.UNIX_SECONDS_SINCE_1970_UTC_VALUE;
         }
 
         return GrpcServer.SemanticType.DEFAULT_VALUE;
+    }
+
+    static public void addNumber(GrpcServer.DataChunk.Builder dataChunkBuilder, double numberValue){
+        dataChunkBuilder.addNumberCodes(dataChunkBuilder.getDoubleBucketCount());
+        dataChunkBuilder.addDoubleBucket(numberValue);
+        dataChunkBuilder.addStringCodes(-1);
+    }
+
+    static public void addInteger(GrpcServer.DataChunk.Builder dataChunkBuilder, long intValue){
+        dataChunkBuilder.addNumberCodes(-2);
+        dataChunkBuilder.addNumberCodes(intValue);
+        dataChunkBuilder.addStringCodes(-1);
+    }
+
+    static public void addString(GrpcServer.DataChunk.Builder dataChunkBuilder, String stringValue){
+        if(stringValue == null){
+            addNull(dataChunkBuilder);
+        }
+        else if(stringValue == ""){
+            dataChunkBuilder.addStringCodes(-2);
+            dataChunkBuilder.addNumberCodes(-1);
+        }
+        else {
+            int nextIndexInStringBucket = dataChunkBuilder.getStringBucketCount();
+            dataChunkBuilder.addStringCodes(nextIndexInStringBucket);
+            dataChunkBuilder.addStringBucket(stringValue);
+            dataChunkBuilder.addNumberCodes(-1);
+        }
+    }
+
+    static public void addNull(GrpcServer.DataChunk.Builder dataChunkBuilder){
+        dataChunkBuilder.addStringCodes(-1);
+        dataChunkBuilder.addNumberCodes(-1);
     }
 }
