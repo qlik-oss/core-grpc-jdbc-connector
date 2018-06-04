ebc5c4dca65c1623e283ef6c9f9b81bfe9424bc5
Stefan Enberg <Stefan.Enberg@qlik.com>
2018-06-04 09:00:28 +0200

    Fixed switch instead of if

--- src/main/java/qlik/jdbc/connector/ConnectorImpl.java
+++ src/main/java/qlik/jdbc/connector/ConnectorImpl.java
@@ -48,8 +48,6 @@ public class ConnectorImpl
 
         try{
             //TODO: Make driver configurable from the outside
-
-
             System.out.println("Connecting to database...");
             //TODO: The connection info should be exstracted from the connection string
             conn = DriverManager.getConnection("jdbc:postgresql://localhost:54321/postgres", "postgres", "postgres");
@@ -75,60 +73,47 @@ public class ConnectorImpl
                 rowCount++;
 
                 for(int i = 0; i < columnCount; i++){
-
                     try {
-                        //TODO: Cleanup this and make an extract logic to its own thread
-                        //need to looka at the orignal type
-//
-//                        if(rsmd.getColumnType(i+1) == Types.CHAR){
-//                            rs.getDouble(i+1 );
-//                            rs.getFloat( i+1 );
-//                        }
-
-                        //System.out.println(rsmd.getColumnType(i+1) + " - " + dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue());
-
-//                        rsmd.getColumnType(i)
-                        if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TEXT_VALUE) {
-                            TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i+1));
-                        }
-
-                        else if (rsmd.getColumnType(i+1) == Types.TINYINT ){
-                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getInt(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.INTEGER ){
-                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getInt(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.BIGINT ){
-                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getLong(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.BOOLEAN){
-                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getBoolean(i+1) ? 1 : 0);
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.BIT){
-                            TypeConversionUtil.addInteger(dataChunkBuilder, rs.getBoolean(i+1) ? 1 : 0);
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.DECIMAL ){
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.NUMERIC ){
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.DOUBLE ){
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.FLOAT ){
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getFloat(i+1));
-                        }
-                        else if (rsmd.getColumnType(i+1) == Types.REAL ){
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getFloat(i+1));
-                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.DATE_VALUE) {
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDate(i+1).getTime() / 1000);
-                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TIME_VALUE) {
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTime(i+1).getTime() / 1000);
-                        } else if (dataResponse.getFieldInfo(i).getFieldAttributesOrBuilder().getTypeValue() == FieldAttrType.TIMESTAMP_VALUE) {
-                            TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTimestamp(i+1).getTime() / 1000);
-                        } else {
-                            TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i+1));
+                        switch (rsmd.getColumnType(i+1)) {
+                            case Types.CHAR:
+                            case Types.VARCHAR:
+                            case Types.LONGNVARCHAR:
+                            case Types.LONGVARCHAR:
+                            case Types.NCHAR:
+                            case Types.NVARCHAR:
+                                TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i + 1));
+                                break;
+                            case Types.TINYINT:
+                            case Types.INTEGER:
+                                TypeConversionUtil.addInteger(dataChunkBuilder, rs.getInt(i + 1));
+                                break;
+                            case Types.BIGINT:
+                                TypeConversionUtil.addInteger(dataChunkBuilder, rs.getLong(i + 1));
+                                break;
+                            case Types.BOOLEAN:
+                            case Types.BIT:
+                                TypeConversionUtil.addInteger(dataChunkBuilder, rs.getBoolean(i + 1) ? 1 : 0);
+                                break;
+                            case Types.DECIMAL:
+                            case Types.NUMERIC:
+                            case Types.DOUBLE:
+                                TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDouble(i + 1));
+                                break;
+                            case Types.FLOAT:
+                            case Types.REAL:
+                                TypeConversionUtil.addNumber(dataChunkBuilder, rs.getFloat(i + 1));
+                                break;
+                            case Types.DATE:
+                                TypeConversionUtil.addNumber(dataChunkBuilder, rs.getDate(i + 1).getTime() / 1000);
+                                break;
+                            case Types.TIME:
+                                TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTime(i + 1).getTime() / 1000);
+                                break;
+                            case Types.TIMESTAMP:
+                                TypeConversionUtil.addNumber(dataChunkBuilder, rs.getTimestamp(i + 1).getTime() / 1000);
+                                break;
+                            default:
+                                TypeConversionUtil.addString(dataChunkBuilder, rs.getString(i + 1));
                         }
                     }
                     catch(Exception e){
@@ -170,13 +155,9 @@ public class ConnectorImpl
         }
 
         responseObserver.onCompleted();
-
-        System.out.println("Data sent!");
     }
 
     public GetDataResponse getDataResponseMetadata(){
-
-        System.out.println("Fetching header");
         return initialMetadata.get();
     }
 }
